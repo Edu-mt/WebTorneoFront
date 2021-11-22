9dfcef311784785c2736e1ae63f6a8ff2efaa87f
Edu-mt <dudumunoz@gmail.com>
2021-11-22 11:00:26 +0100

    arreglado lo de Noticias

--- src/components/PagPrincipal/Noticias/Noticias.js
+++ src/components/PagPrincipal/Noticias/Noticias.js
@@ -8,26 +8,29 @@ export default function Noticias() {
 
        
     useEffect( async() => {       
-        const torneoTraido = await traerTorneo();                
-        const arrayNoticias = new Array();
-        console.log("Esto es torneoTraido",  torneoTraido);
-        console.log("Esto es torneoTraido.ganadores:",  torneoTraido[0].ganadores);
-        console.log("Esto es torneoTraido.ganadores.length:",  torneoTraido[0].ganadores.length);
-        setNombreTorneo(torneoTraido[0].nombreTorneo);
-        for (let a=0 ; a < torneoTraido[0].ganadores.length; a++) {
-            let g = torneoTraido[0].ganadores[a].indice
-            var info = {ganador: torneoTraido[0].ganadores[a].resultados , equipo1:torneoTraido[0].arrayPartidas[g].equipo1 , equipo2:torneoTraido[0].arrayPartidas[g].equipo2}
-            arrayNoticias.push(info);
-            console.log("esto es arrayNoticias:",  arrayNoticias);
-        }
-        setInfoNoticias(arrayNoticias);
-        console.log("esto es infoNoticias:",  infoNoticias);
-      }, []);         
+        const torneoTraido = await traerTorneo();  
+        if (torneoTraido != 0){
+            const arrayNoticias = new Array();
+            console.log("Esto es torneoTraido",  torneoTraido);
+            console.log("Esto es torneoTraido.ganadores:",  torneoTraido[0].ganadores);
+            console.log("Esto es torneoTraido.ganadores.length:",  torneoTraido[0].ganadores.length);
+            setNombreTorneo(torneoTraido[0].nombreTorneo);
+            for (let a=0 ; a < torneoTraido[0].ganadores.length; a++) {
+                let g = torneoTraido[0].ganadores[a].indice
+                var info = {ganador: torneoTraido[0].ganadores[a].resultados , equipo1:torneoTraido[0].arrayPartidas[g].equipo1 , equipo2:torneoTraido[0].arrayPartidas[g].equipo2}
+                arrayNoticias.push(info);
+                console.log("esto es arrayNoticias:",  arrayNoticias);
+            }
+            setInfoNoticias(arrayNoticias);
+            console.log("esto es infoNoticias:",  infoNoticias);
+         }
+    }, []);         
     
     return (
         <div>
-            Noticias del {nombreTorneo}
-            <div>                
+            Noticias
+            <div>      
+                <div>{nombreTorneo}</div>          
                 {infoNoticias.map((info) => {
                     return (
                     <>                  
