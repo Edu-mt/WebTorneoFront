2fadb7a49e4d62b59b5c356e6bf62cf50a7fb535
enri364 <enri364@gmail.com>
2021-11-25 20:09:43 +0100

    footer cambio de imagen en movily tamaño

--- src/components/PagPrincipal/CajaDerecha/CajaDerecha.js
+++ src/components/PagPrincipal/CajaDerecha/CajaDerecha.js
@@ -5,8 +5,7 @@ export default function CajaDerecha() {
     return (
         <div>
         <div className="cajaDrchaPrincipal">
-            {/* <h1>Caja derecha</h1>
-            <div></div> */}
+
         </div>
         </div>
     )
