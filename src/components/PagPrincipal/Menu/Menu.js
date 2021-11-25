7eee3e6df8ffd1a95131d6a54316059bfb0393f6
enri364 <enri364@gmail.com>
2021-11-25 16:17:30 +0100

    comienzo de cambios en App.css cuerpo y correcion hover header boton cerrar sesion

--- src/components/PagPrincipal/Menu/Menu.js
+++ src/components/PagPrincipal/Menu/Menu.js
@@ -13,7 +13,12 @@ function Menu () {
         <div><Link to="/Equipos"><button className="btnMenu">EQUIPOS</button></Link></div>
         <div><Link to="/Perfil"><button className="btnMenu">PERFIL</button></Link></div>
         <div><Link to="/Calendario"><button className="btnMenu">CALENDARIO</button></Link></div>
+        <div><Link to="/Noticias"><button className="btnMenuResponsiveMovil"></button></Link></div>
+        <div><Link to="/Equipos"><button className="btnMenuResponsiveMovil"></button></Link></div>
+        <div><Link to="/Perfil"><button className="btnMenuResponsiveMovil"></button></Link></div>
+        <div><Link to="/Calendario"><button className="btnMenuResponsiveMovil"></button></Link></div>
        <div className="imgIzq"></div>
+       <div className="imgIzqResponsiveMovil"></div>
        </div>    
         </>
       );
