afeeb474a27b06a366dcd3d165d2a5157b6646d1
enri364 <enri364@gmail.com>
2021-11-25 17:37:01 +0100

    la caja de menu principal ha sido borrada porque ya estaba en app.js cubriendo <Menu> en el return y ahora se llama Menu ese className

--- src/components/PagPrincipal/Menu/Menu.js
+++ src/components/PagPrincipal/Menu/Menu.js
@@ -7,19 +7,19 @@ function Menu () {
       
       return (
         <>
-       <div className="MenuPrincipal">
+    
          {/* <h1 className="Menu">MENU</h1> */}
         <div><Link to="/Noticias"><button className="btnMenu">NOTICIAS</button></Link></div>
         <div><Link to="/Equipos"><button className="btnMenu">EQUIPOS</button></Link></div>
         <div><Link to="/Perfil"><button className="btnMenu">PERFIL</button></Link></div>
         <div><Link to="/Calendario"><button className="btnMenu">CALENDARIO</button></Link></div>
-        <div><Link to="/Noticias"><button className="btnMenuResponsiveMovil"></button></Link></div>
-        <div><Link to="/Equipos"><button className="btnMenuResponsiveMovil"></button></Link></div>
-        <div><Link to="/Perfil"><button className="btnMenuResponsiveMovil"></button></Link></div>
-        <div><Link to="/Calendario"><button className="btnMenuResponsiveMovil"></button></Link></div>
+        <div><Link to="/Noticias"><button className="btnNoticiasResponsiveMovil"></button></Link></div>
+        <div><Link to="/Equipos"><button className="btnNoticiasResponsiveMovil"></button></Link></div>
+        <div><Link to="/Perfil"><button className="btnNoticiasResponsiveMovil"></button></Link></div>
+        <div><Link to="/Calendario"><button className="btnNoticiasResponsiveMovil"></button></Link></div>
        <div className="imgIzq"></div>
        <div className="imgIzqResponsiveMovil"></div>
-       </div>    
+     
         </>
       );
 }
