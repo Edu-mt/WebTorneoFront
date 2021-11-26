d7f7e57f5daaecb18176ce11d9a96f576268c264
Lidiafg91 <lidiaflores.gomez91@gmail.com>
2021-11-26 21:12:26 +0100

    cambios en equipos header y menu

--- src/components/PagPrincipal/Menu/Menu.js
+++ src/components/PagPrincipal/Menu/Menu.js
@@ -12,9 +12,9 @@ function Menu () {
         <div><Link to="/Perfil"><button className="btnMenu">PERFIL</button></Link></div>
         <div><Link to="/Calendario"><button className="btnMenu">CALENDARIO</button></Link></div>
         <div><Link to="/Noticias"><button className="btnNoticiasResponsiveMovil"></button></Link></div>
-        <div><Link to="/Equipos"><button className="btnEquiposResponsiveMovil"></button></Link></div>
-        <div><Link to="/Perfil"><button className="btnPerfilResponsiveMovil"></button></Link></div>
-        <div><Link to="/Calendario"><button className="btnCalendarioResponsiveMovil"></button></Link></div>
+        <div><Link to="/Equipos"><button className="btnNoticiasResponsiveMovil"></button></Link></div>
+        <div><Link to="/Perfil"><button className="btnNoticiasResponsiveMovil"></button></Link></div>
+        <div><Link to="/Calendario"><button className="btnNoticiasResponsiveMovil"></button></Link></div>
        <div className="imgIzq"></div>
        <div className="imgIzqResponsiveMovil"></div>   
         </>
